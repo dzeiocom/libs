dd8515cc9b7a94ecf3e15515e051b5de764ce2cc
Florian Bouillon <florian.bouillon@delta-wings.net>
2020-07-02 16:58:30 +0200

    Added Queue System

---
+++ packages/queue/Queue.ts
@@ -0,0 +1,49 @@
+import Logger from "@dzeio/logger"
+
+export default class Queue {
+	private queue = 0
+	private isPaused = false
+	
+	public constructor(
+		private maxQueueLength = 5,
+		private timeToWait = 500
+	) {}
+
+	public pause() {
+		this.isPaused = true
+	}
+
+	public start() {
+		this.isPaused = false
+	}
+
+	public updateCurrentQueueLength(len: number) {
+		this.queue = len
+	}
+
+	public async add<T = any>(promise: Promise<T>) {
+		while (this.queue >= this.maxQueueLength || this.isPaused) {
+			await new Promise((res) => setTimeout(res, this.timeToWait))
+		}
+		this.updateCurrentQueueLength(this.queue+1)
+		promise
+			.then(() => {
+				this.updateCurrentQueueLength(this.queue-1)
+			}).catch(() => {
+				this.updateCurrentQueueLength(this.queue-1)
+			})
+	}
+
+	public async waitEnd() {
+		let currentQueue = this.queue
+		while (this.queue !== 0) {
+			await new Promise((res) => setTimeout(() => {
+				if (currentQueue !== this.queue) {
+					Logger.log('PromiseQueue', this.queue, 'remaining in queue')
+					currentQueue = this.queue
+				}
+				res()
+			}, this.timeToWait))
+		}
+	}
+}
