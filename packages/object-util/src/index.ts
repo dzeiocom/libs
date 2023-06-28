4f37026c5b3f64a9b4e417fe2191e8c8c96f6235
Florian BOUILLON <f.bouillon@aptatio.com>
2023-06-28 14:40:50 +0200

    feat(object-util): bump 1.6.0
    Signed-off-by: Florian BOUILLON <f.bouillon@aptatio.com>

--- packages/object-util/src/index.ts
+++
@@ -1,9 +0,0 @@
-import ObjectUtil from './ObjectUtil'
-
-ObjectUtil.objectLoop(ObjectUtil, (fn, key) => {
-	// @ts-expect-error
-	window[key as any] = fn
-})
-
-// @ts-expect-error
-window.ObjectUtil = ObjectUtil
